5e68038c817fa91ccea2a20880edaf58e6c11b81
Carlos Costa <carloscosta.cmcc@gmail.com>
2015-06-10 18:41:08 +0100

    Updated readme and improved clean script for creating symlinks that have spaces in the folders names.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # dynamic_robot_localization_dissertation_abstracts
-Abstracts for dissertation on robot self-localization systems for 3-6 DoF
+Abstracts for the dissertation [Robot Self-Localization in Dynamic Environments](https://www.researchgate.net/publication/277752192_Robot_Self-Localization_in_Dynamic_Environments) with implementation available in the [dynamic_robot_localization](https://github.com/carlosmccosta/dynamic_robot_localization) git repository.
--- clean.sh
+++ clean.sh
@@ -11,11 +11,11 @@ rm -rf build-pt
 mkdir build-en
 mkdir build-pt
 
-ln -s `pwd`/references.bib `pwd`/build-en/references.bib
-ln -s `pwd`/references.bib `pwd`/build-pt/references.bib
+ln -s "`pwd`/references.bib" "`pwd`/build-en/references.bib"
+ln -s "`pwd`/references.bib" "`pwd`/build-pt/references.bib"
 
-ln -s `pwd`/unsrt-pt.bst `pwd`/build-en/unsrt-pt.bst
-ln -s `pwd`/unsrt-pt.bst `pwd`/build-pt/unsrt-pt.bst
+ln -s "`pwd`/unsrt-pt.bst" "`pwd`/build-en/unsrt-pt.bst"
+ln -s "`pwd`/unsrt-pt.bst" "`pwd`/build-pt/unsrt-pt.bst"
 
 
 
